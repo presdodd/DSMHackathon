1a67aeaa580a481215840756b518259b80c673d0
presdodd <presdodd@uiowa.edu>
2024-02-18 07:50:56 -0600

    Added link to title

--- frontend/app/components/header.js
+++ frontend/app/components/header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className="flex items-center mb-4">
       <img src="/logo.png" alt="Logo" className="h-8 w-8 mr-2" />
-      <h1 className="text-2xl font-bold">Fishy Fellas</h1>
+      <h1 className="text-2xl font-bold"><Link href="/"k>FishySolutions</Link></h1>
       <Link className="ml-4" href="/about">
         About
       </Link>
